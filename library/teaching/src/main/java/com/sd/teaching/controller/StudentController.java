cc435423b6d881e11a4c777621bce99503339da3
苏丹 <sudan@yxqiche.com>
2020-07-14 17:24:53 +0800

    用户操作日志记录到mongo中

--- library/teaching/src/main/java/com/sd/teaching/controller/StudentController.java
+++ library/teaching/src/main/java/com/sd/teaching/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.sd.teaching.controller;
 
+import com.sd.teaching.common.annotions.BehaviorLog;
 import com.sd.teaching.dto.InvokeResult;
 import com.sd.teaching.model.StudentInfo;
 import com.sd.teaching.service.StudentService;
@@ -23,24 +24,28 @@ public class StudentController {
     private StudentService studentService;
 
     @PostMapping("/add")
+    @BehaviorLog("新增学生信息")
     public InvokeResult<Void> add(@RequestBody StudentInfo studentInfo){
         studentService.add(studentInfo);
         return InvokeResult.success();
     }
 
     @PostMapping("/update")
+    @BehaviorLog("更新学生信息")
     public InvokeResult<Void> update(@RequestBody StudentInfo studentInfo){
         studentService.update(studentInfo);
         return InvokeResult.success();
     }
 
     @GetMapping("/queryAll")
+    @BehaviorLog("查询学生信息")
     public InvokeResult<List<StudentInfo>> queryAll(){
         List<StudentInfo> list = studentService.queryAll();
         return InvokeResult.success(list);
     }
 
     @GetMapping("/delete/{stuNo}")
+    @BehaviorLog("删除学生信息")
     public InvokeResult<Void> delete(@PathVariable String stuNo){
         studentService.delete(stuNo);
         return InvokeResult.success();
